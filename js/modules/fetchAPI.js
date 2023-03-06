365e040a624ed1fe651f373913dea07bce10e1fb
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-03-06 15:33:09 +0100

    Tried to work with country filtering

--- js/modules/fetchAPI.js
+++ js/modules/fetchAPI.js
@@ -1,72 +1,64 @@
+import renderEvents from "./renderEvents.js";
+// import filterEvents from "./filterEvents.js";
+// import { currentDropdownValue } from "./filterEvents.js";
+// const dropdown = currentDropdownValue()
+import { API_KEY } from "../../env.js";
+let currentDropdownValue = 'SE';
 export default async function fetchAPI() {
 	//data model
-	const apiKey = 'vRUAnvHH8eelbE3N89Nh3pGKlnQE3VGc';
+	let countryCode = currentDropdownValue;
+	const apiKey = API_KEY;
 	const baseURL = 'https://app.ticketmaster.com/discovery/v2/events.json';
 	const options = {
 		method: "GET"
 	};
-	const endpoint	= `${baseURL}?apikey=${apiKey}` ;
+	const endpoint	= `${baseURL}?apikey=${apiKey}&countryCode=${countryCode}` ;
+	console.log(endpoint)
 
-	const response = await fetch(endpoint);
+	const response = await fetch(endpoint, options);
 
 	try {
 		await handleResponse(response)
 	} catch (error) {
 		console.log(error)
-		// handleError(error)
 	}
 }
 
 async function handleResponse(response) {
-	const eventsSection = document.querySelector('.events-section');
 	if (response.ok) {
 		const output = await response.json();
 		//do what the api is going to do here VVVVVVV
 		const eventList = output._embedded.events;
 		eventList.forEach(element => {
-			renderHTML(element);
+			renderEvents(element);
 		});
-		// output.forEach(output._embedded.events.name)
-		// function renderAll() {
-		// 	eventsSection.innerHTML = '';
-		// 	output.forEach(() => {
-		// 		renderHTML();
-		// 	});
-		// }
-
-		
-		// renderAll();
-
-		////
-		// renderCard(output._embedded.events.name)
 
 
-		function renderHTML(element) {
-			const eventCardLink = document.createElement('a');
-			eventCardLink.className = 'event__card-link'
-			eventCardLink.href = `${element.url}`
-			eventCardLink.setAttribute('target', '_blank')
-			eventsSection.appendChild(eventCardLink)
-
-			const eventCard = document.createElement('div');
-			eventCard.className = 'event__card';
-			eventCardLink.appendChild(eventCard);
-			
-			const eventImageBox = document.createElement('div');
-			eventImageBox.className = 'event__image-box';
-			const eventImage = document.createElement('img');
-			eventImage.setAttribute('src', `${element.images[3].url}`);
-			eventImageBox.appendChild(eventImage);
-			eventCard.appendChild(eventImageBox);
-			
-			const eventTitle = document.createElement('h2');
-			eventTitle.className = 'event__title';
-			eventCard.appendChild(eventTitle);
-			eventTitle.textContent = `${element.name}`;
+		const countryDropdown = document.getElementById('country');
+	
+		countryDropdown.addEventListener('change', handleDropdown);
+		function handleDropdown() {
+			emptyHTML()
+			filterEvents()
+			fetchAPI()
+			eventList.forEach(element => {
+				renderEvents(element);
+			});
+		}
+		function filterEvents() {
+			// let currentDropdownValue = ''
+			currentDropdownValue = countryDropdown.value;
+			console.log(currentDropdownValue);
+			return currentDropdownValue;
 		}
 
+		function emptyHTML() {
+			const eventsSection = document.querySelector('.events-section');
+			eventsSection.innerHTML = '';
+		}
 
-		//
+		// filterEvents()
+		
 	} else if (response.status === 404) {
 		throw new Error('Url not existing');
 	} else if (response.status === 401) {
@@ -77,8 +69,3 @@ async function handleResponse(response) {
 		throw new Error('Something went wrong');
 	}
 }
-
-// function handleError(error) {
-// 	warningElement.classList.toggle('hidden');
-// 	warningElement.textContent = error.message;
-// }
